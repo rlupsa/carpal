35211f9f8e7c6ab6d64201e4c82f596c2b4a29e0
rlupsa <rlupsa@neptun.lupsa-home>
2023-09-01 21:28:38 +0300

    Added copyright information

--- tests/Test.cpp
+++ tests/Test.cpp
@@ -1,2 +1,6 @@
+// Copyright Radu Lupsa 2023
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.txt or copy at  https://www.boost.org/LICENSE_1_0.txt )
+
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch2/catch.hpp>
--- tests/TestTimer.cpp
+++ tests/TestTimer.cpp
@@ -1,3 +1,7 @@
+// Copyright Radu Lupsa 2023
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.txt or copy at  https://www.boost.org/LICENSE_1_0.txt )
+
 #include "carpal/Future.h"
 #include "carpal/Timer.h"
 #include "carpal/ThreadPool.h"
